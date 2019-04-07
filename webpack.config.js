c9b1ee3814c7d65d1b0c2927391162118933e242
davidelo <daw.kielbik@gmail.com>
2019-04-07 15:56:39 +0200

    finished webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const miniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: './src/build/js/index.js',
+    output: {
+        path: path.resolve(__dirname, './src/app'),
+        filename: 'script.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.html$/,
+                use: [{
+                    loader: "html-loader",
+                    options: { minimize: true }
+                }]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use:  [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            template: './src/build/index.html',
+            filename: './index.html'
+        }),
+        new miniCssExtractPlugin({
+            filename: '[name].css',
+            chunkFilename: '[id].css'
+        })
+    ]
+};
