681e5320f10a4fb707d3d3a80ebc131a389df906
davidelo <daw.kielbik@gmail.com>
2019-04-07 21:48:51 +0200

    finished navbar for mobile

--- src/build/js/index.js
+++ src/build/js/index.js
@@ -1,1 +1,9 @@
 import '../css/main.scss';
+
+const dropdownBtn = document.getElementById('dropdownBtn');
+const menu = document.getElementById('siteMenu');
+
+dropdownBtn.addEventListener('click', () => {
+    dropdownBtn.classList.toggle('open');
+    menu.classList.toggle('menu__list--active');
+});
